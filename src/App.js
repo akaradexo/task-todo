52b3d66778823cec35cde7994c4a0eaf034e14e5
Ashutosh Karade <69238714+xR0v3Rx@users.noreply.github.com>
2021-12-17 17:53:23 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
+import List from './components/List'
+import { v4 as uuid4 } from 'uuid';
+import Modal from 'react-modal';
+const App = () => {
+  const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
 
-function App() {
+  const [task,setTask] = useState('');
+  const [list,setList]=useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  
+  const openModal=()=> {
+    setIsOpen(true);
+  }
+
+ 
+
+  const closeModal=()=> {
+    setIsOpen(false);
+  }
+  // const [alert,setAlert]=useState()
+
+
+
+  const selectTask=(e)=>{
+    const objIndex = list.findIndex((obj) => obj.id === e);
+    list[objIndex].completed = true;
+    const newList=[...list];
+    setList(newList);
+  }
+  const deselectTask=(e)=>{
+    const objIndex = list.findIndex((obj) => obj.id === e);
+    list[objIndex].completed = false;
+    const newList=[...list];
+    setList(newList);
+  }
+  const deleteTask=(id)=>{
+    const updatedList=list.filter(item=>item.id!==id);
+    setList(updatedList)
+  }
+
+  const submitHandler=(e)=>{
+    e.preventDefault();
+    
+      //add task
+      const newTask={id:uuid4(),title:task, completed:false}
+      setList([...list,newTask])
+      setTask('');
+      closeModal()
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section>
+      <div className="container">
+        <div className="header">Website todo</div>
+        {list.length > 0 && <List items={list} deleteTask={deleteTask} selectTask={selectTask} deselectTask={deselectTask}/>}
+        <button className='btn-modal' onClick={openModal}>+ New task</button>
+        <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+          
+        <form onSubmit={submitHandler} > 
+          <input 
+          type="text"
+          placeholder='eg. buy grocery...' 
+          className='custom-input'
+          value={task}
+          onChange={(e)=>setTask(e.target.value)}
+          />
+          <button className='btn-add' type='submit'>Add</button>
+        </form>
+        </Modal>
+      </div>
+    </section>
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/List.js
@@ -0,0 +1,43 @@
+import React,{useState} from 'react'
+import { FiTrash2 } from 'react-icons/fi';
+
+const List = ({items,deleteTask,selectTask,deselectTask}) => {
+
+
+
+
+  return (
+    <div className='list-container'>
+     {items.map((item)=>{
+        const{id,title,completed}=item;
+        return(
+          <div className='task-container' key={id}>
+            {completed ? (
+              <>
+                  <div className="task-list">
+                    <input type="checkbox" className="checkbox-round" onClick={()=>deselectTask(id)} />
+                    <p style={{ textDecoration: 'line-through' }}>{title}</p> 
+                  </div>
+                  <button 
+                  className='btn-delete' 
+                  onClick={()=>deleteTask(id)}>
+                  <FiTrash2/></button>
+              </>
+           ):(
+              <>
+                  <div className="task-list">
+                    <input type="checkbox" className="checkbox-round" onClick={()=>selectTask(id)} />
+                    <p>{title}</p> 
+                  </div>
+                  
+              </>
+            )}
+          </div>
+        );
+      })}
+    </div>
+     
+  )
+}
+
+export default List
